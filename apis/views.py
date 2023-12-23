97b811a80dbe5e8f86316d07a435145f9091301d
bakhtiiartashbolotov <124582613+bakhtiiartashbolotov@users.noreply.github.com>
2023-12-23 11:59:49 +0600

    CRUD with REST API

--- apis/views.py
+++ apis/views.py
@@ -4,6 +4,9 @@ from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
 
-class BlogAPIView(generics.ListAPIView): 
+class BlogAPIView(generics.ListCreateAPIView): 
+    queryset = Post.objects.all() 
+    serializer_class = PostSerializer
+class BlogDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all() 
     serializer_class = PostSerializer
